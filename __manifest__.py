6347368f411fd7883b6a89aee9911ceafb20c7d7
Azlan <aburney@knysys.com>
2022-06-20 05:42:35 +0500

    Customer API integration for create.

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['delivery'],
+    'depends': ['base', 'delivery', 'sale'],
 
     # always loaded
     'data': [
--- models/customer.py
+++ models/customer.py
@@ -4,6 +4,7 @@ import json
 import random
 import string
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class ResCountryState(models.Model):
@@ -37,7 +38,7 @@ class SmarttshipSync(models.Model):
         response = requests.request("GET", url, headers=headers, data=payload)
         response_dict = json.loads(response.text)
         if response_dict.get('Success') == True:
-            for city in response.get('Cities'):
+            for city in response_dict.get('Cities'):
                 city_available = self.env['res.city.smarttship'].search([('city_id', '=', int(city.get('CityId'))), ('city_name', '=', str(city.get('CityName')))])
                 if not city_available:
                     city_rec = self.env['res.city.smarttship'].create({
@@ -76,7 +77,85 @@ class SmarttshipSync(models.Model):
 class ResCitySmartship(models.Model):
     _name = "res.city.smarttship"
 
+
     city_id = fields.Integer(string='City ID')
     city_name = fields.Char(string='City Name')
 
 
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    smarttship_customer_id = fields.Integer(string='Smarttship Customer ID')
+    smarttship_customer = fields.Boolean(string='Is Smarttship Customer')
+    special_instructions = fields.Char(string='Special Instructions')
+    broker_name = fields.Char(string='Broker Name')
+    secondary_phone = fields.Char(string='Secondary Phone')
+    contact_name = fields.Char(string='Contact Name')
+    opening_time = fields.Datetime(string='Opening Time')
+    closing_time = fields.Datetime(string='Closing Time')
+    account_no = fields.Char(string='Account No')
+    primary_contact_position = fields.Char(string='Primary Contact Position')
+    primary_contact_phone = fields.Char(string='Primary Contact Phone')
+    secondary_contact_name = fields.Char(string='Secondary Contact Name')
+    secondary_contact_position = fields.Char(string='Secondary Contact Position')
+    is_active = fields.Boolean(string='Is Active')
+    notify = fields.Boolean(string='Is Active')
+    phone_extenstion = fields.Char(string='Phone Extension')
+
+    @api.model
+    def create(self, vals):
+        rec = super(ResPartner, self).create(vals)
+        print('creating customer')
+        if rec.smarttship_customer == True:
+            print('development of customer')
+            url = "https://api.sandbox.smarttshipping.ca/api/carrierApi/AddOrEditCustomer"
+            headers = {
+                'apikey': '7474CAE8-35BA-47DE-983D-2DE16EDEB118',
+                'Content-Type': 'application/json'
+            }
+            city_id = self.env['res.city.smarttship'].search([('city_name', '=', rec.city)])
+            state_id = rec.state_id.smarttship_state_id
+            country_id = rec.country_id.id
+            zip_code = rec.zip
+            email = rec.email
+            primary_phone = rec.phone
+            if city_id and state_id and country_id and zip_code and email and primary_phone:
+                payload = json.dumps({
+                        "CustomerID": 0,
+                        "CustomerName": rec.name,
+                        "Address": rec.street + ' ' + rec.street2,
+                        "CityID": city_id.id,
+                        "StateID": state_id,
+                        "CountryID": country_id,
+                        "ZipCode": zip_code,
+                        "EmailID": email,
+                        "PrimaryPhone": primary_phone,
+                        "SpecialInstructions": rec.special_instructions if rec.special_instructions else False,
+                        "BrokerName": rec.broker_name if rec.broker_name else False,
+                        "SecondaryPhone": rec.secondary_phone if rec.secondary_phone else False,
+                        "Website": rec.website if rec.website else False,
+                        "ContactName": rec.contact_name if rec.contact_name else False,
+                        "OpeningTime": str(rec.opening_time.strftime('%Y-%m-%dT%H:%M:%S')) if rec.opening_time else False,
+                        "ClosingTime": str(rec.closing_time.strftime('%Y-%m-%dT%H:%M:%S')) if rec.closing_time else False,
+                        "AccountNo": rec.account_no if rec.account_no else False,
+                        "PrimaryContactPosition": rec.primary_contact_position if rec.primary_contact_position else False,
+                        "PrimaryContactPhone": rec.primary_contact_phone if rec.primary_contact_phone else False,
+                        "SecondaryContactName": rec.secondary_contact_name if rec.secondary_contact_name else False,
+                        "SecondaryContactPosition": rec.secondary_contact_position if rec.secondary_contact_position else False,
+                        "AdditionalNotes": rec.comment if rec.comment else False,
+                        "IsActive": rec.is_active if rec.is_active else False,
+                        "Notify": rec.notify if rec.notify else False,
+                        "PhoneExtension": rec.phone_extenstion if rec.phone_extenstion else False
+                    })
+
+                response = requests.request("POST", url, headers=headers, data=payload)
+                response_dict = json.loads(response.text)
+                if response_dict.get('Success') == True:
+                    rec.smarttship_customer_id = response_dict.get('Customer').get('CustomerID')
+                    print('response successful')
+                else:
+                    raise ValidationError(_(str(response_dict.get('Message'))))
+            else:
+                raise ValidationError(_(str('Some required fields in Smarttship are empty.')))
+        return rec
